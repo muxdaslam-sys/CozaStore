28b9f886f184cca6b04ca654bdcb17d55631b281
Aslam <muxd.aslam@gmail.com>
2026-07-27 16:21:21 +0530

    Develop- Fix User Pages Prolems (#3)
    * Fix product page
    
    * Fix category filter visual flicker during partial postback

--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -294,6 +294,15 @@ namespace Ecom_Project
         protected global::System.Web.UI.WebControls.TextBox stb7;
 
         /// <summary>
+        /// RequiredFieldValidator7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator7;
+
+        /// <summary>
         /// stb7_una control.
         /// </summary>
         /// <remarks>
--- User/Product.aspx.cs
+++ User/Product.aspx.cs
@@ -4,14 +4,110 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Ecom_Project.User
 {
     public partial class Product : System.Web.UI.Page
     {
+        ConClass ob = new ConClass();
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (!IsPostBack)
+            {
+                BindCategories();
+                int categoryId = Convert.ToInt32(Request.QueryString["categoryId"]);
+                if (categoryId!= 0)
+                {
+                    BindProductsByCategory(categoryId);
+                }
+                else
+                {
+                    BindAllProducts();
+                }
+            }
+        }
+
+        private void BindCategories()
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = @"SELECT Category_id, Category_name
+                                FROM Category_tab
+                                WHERE Category_status = @status";
+
+            cmd.Parameters.AddWithValue("@status", "available");
+
+            DataSet ds = ob.SP_Adapter(cmd);
+
+            DL_cat.DataSource = ds;
+            DL_cat.DataBind();
+        }
+
+        // Show all products
+        private void BindAllProducts()
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = @"SELECT Product_id, Product_image,Product_name,Product_description,Product_price
+                                FROM Product_tab
+                                WHERE Product_status = @status";
+
+            cmd.Parameters.AddWithValue("@status", "available");
+
+            DataSet ds = ob.SP_Adapter(cmd);
+
+            DL_Product.DataSource = ds;
+            DL_Product.DataBind();
+        }
+
+        // Show products from selected category
+        private void BindProductsByCategory(int categoryId)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = @"SELECT Product_id,
+                               Product_image,
+                               Product_name,
+                               Product_description,
+                               Product_price
+                        FROM Product_tab
+                        WHERE Product_status = @status
+                        AND Category_id = @CategoryId";
+
+            cmd.Parameters.AddWithValue("@status", "available");
+            cmd.Parameters.AddWithValue("@CategoryId", categoryId);
+
+            DataSet ds = ob.SP_Adapter(cmd);
+
+            DL_Product.DataSource = ds;
+            DL_Product.DataBind();
+        }
+
+        protected void BtnAll_Click(object sender, EventArgs e)
+        {
+            BindAllProducts();
+        }
+
+        protected void ImageButton1_Command(object sender, CommandEventArgs e)
+        {
+            string ProductId = e.CommandArgument.ToString();
+
+            Response.Redirect("Product-Details.aspx?ProductId=" + ProductId);
+        }
+
+
+        protected void BtnCat_Command(object sender, CommandEventArgs e)
+        {
+            int categoryId = Convert.ToInt32(e.CommandArgument);
+
+            BindProductsByCategory(categoryId);
+        }
+
+        protected void TxtSearch_TextChanged(object sender, EventArgs e)
+        {
+            // Placeholder: code your search text change logic here later
         }
     }
 }
